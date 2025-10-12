a99059d474a9966c04955c515706fa6ddcfeb93a
Marcus Allen <marcusallen@Marcuss-MacBook-Pro.local>
2025-10-12 01:43:00 +0100

    📍 Add Better.org.uk locations config
    - Created locations/better.js
    - Added Highbury Fields (Islington Tennis Centre)
    - Tagged as North London
    - Booking window: 6 days
    - Includes venue and activity slugs for API calls

---
+++ locations/better.js
@@ -0,0 +1,13 @@
+const betterLocations = [
+  {
+    name: "Highbury Fields",
+    venue: "islington-tennis-centre",
+    activity: "highbury-tennis",
+    url: "https://bookings.better.org.uk/location/islington-tennis-centre/highbury-tennis",
+    bookingWindow: 6,
+    tags: ["North"]
+  }
+];
+
+export default betterLocations;
+
