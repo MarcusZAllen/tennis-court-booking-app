53451445c5d45a59f154684b64a279191fdf4a7a
Marcus Allen <marcusallen@Marcuss-MacBook-Pro.local>
2025-10-13 20:16:27 +0100

    🐛 Add Sonner toaster for feedback notifications
    - FeedbackModal uses sonner toast
    - Added Sonner toaster to _app.tsx
    - Now toast notifications will display properly
    - Feedback button should now be visible and functional

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Toaster } from '../components/ui/toaster';
+import { Toaster as Sonner } from 'sonner';
 import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Component {...pageProps} />
       <Toaster />
+      <Sonner />
       <Analytics />
     </>
   );
